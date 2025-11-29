664c32003f2bb89699aad2c4765e024933f1e84b
Joshua Hutson <156043533+hutsonjo@users.noreply.github.com>
2025-11-29 18:25:32 -0500

    Update battle_logic.py

--- battle_logic.py
+++ battle_logic.py
@@ -3,15 +3,15 @@ def battle_logic(player, enemy):
     Calculates the result of a turn of battle between the player and an enemy.
     """
     # Player goes first
-    damage = player['attack'] - enemy['defense']
-    enemy['health'] = max(enemy['health'] - damage, 0)
+    damage = max(player['attack'] - enemy['defense'], 0)
+    enemy['health'] -= damage
     if enemy['health'] <= 0:
         enemy['health'] = 0
         return player, enemy
 
     # Enemy attacks
-    damage = enemy['attack'] - player['defense']
-    player['health'] = max(player['health'] - damage, 0)
+    damage = max(enemy['attack'] - player['defense'], 0)
+    player['health'] -= damage
     if player['health'] < 0:
         player['health'] = 0
     return player, enemy
